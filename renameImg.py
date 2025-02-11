2f87587b5d8beef3accafab1b201e04bfea7b0ca
gnevdv <gnevyshevdv@gmail.com>
2025-02-11 22:44:49 +0300

    renew files

--- renameImg.py
+++ renameImg.py
@@ -5,22 +5,30 @@ from PIL import Image
 # Начальное значение файлов
 counter = 0
 # Исходная папка
-source_dir = './source'
+source_dir = "./source"
 # Новая папка с преобразованными файлами
-out_dir = './output'
+out_dir = "./output"
 # Расширение
-ext = 'jpg'
+ext = "jpg"
 # Качество изображения на выходе
-qlt = 65
+qlt = 70
 
-if (os.path.isdir(out_dir)):
+# Если нужно конвертировать из PNG в JPG ставь True, если нет False
+is_from_png_to_jpg = False
+
+if os.path.isdir(out_dir):
     shutil.rmtree(out_dir)
 os.mkdir(out_dir)
 for root, dirs, files in os.walk(source_dir):
     for filename in files:
-        img = Image.open(f'{source_dir}/{filename}')
-        img.save(f"{out_dir}/{counter}.{ext}",
-                 "JPEG",
-                 optimize=True,
-                 quality=qlt)
+        img = Image.open(f"{source_dir}/{filename}")
+        if is_from_png_to_jpg:
+            bg = Image.new("RGB", img.size, (255, 255, 255))
+            bg.paste(img, img)
+            result_img = bg
+        else:
+            result_img = img
+        result_img.save(
+            f"{out_dir}/{counter}.{ext}", "JPEG", optimize=True, quality=qlt
+        )
         counter += 1
--- resizeSquareImage.py
+++ resizeSquareImage.py
@@ -3,31 +3,27 @@ import shutil
 from PIL import Image
 
 # Исходная папка
-source_dir = './source'
+source_dir = "./source"
 # Новая папка с преобразованными файлами
-out_dir = './output'
+out_dir = "./output"
 # Расширение
-ext = 'jpg'
+ext = "jpg"
 # Требуемое разрешение
 resolution = 600
 
-if (os.path.isdir(out_dir)):
+if os.path.isdir(out_dir):
     shutil.rmtree(out_dir)
 os.mkdir(out_dir)
 for root, dirs, files in os.walk(source_dir):
     for filename in files:
-        last_index = filename.rfind('.')
+        last_index = filename.rfind(".")
         output_file_name = f"{filename[0:last_index]}_{resolution}x{resolution}"
-        img = Image.open(f'{source_dir}/{filename}')
-        new_img = img.resize((resolution, resolution),
-                             Image.Resampling.LANCZOS)
+        img = Image.open(f"{source_dir}/{filename}")
+        new_img = img.resize((resolution, resolution), Image.Resampling.LANCZOS)
         try:
-            dpi = img.info['dpi']
-            new_img.save(f"{out_dir}/{output_file_name}.{ext}",
-                         "JPEG",
-                         dpi=dpi,
-                         quality=50)
+            dpi = img.info["dpi"]
+            new_img.save(
+                f"{out_dir}/{output_file_name}.{ext}", "JPEG", dpi=dpi, quality=50
+            )
         except:
-            new_img.save(f"{out_dir}/{output_file_name}.{ext}",
-                         "JPEG",
-                         quality=50)
+            new_img.save(f"{out_dir}/{output_file_name}.{ext}", "JPEG", quality=50)
