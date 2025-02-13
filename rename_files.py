898811d1bd55b6f841a310cb1f9210628f1328cc
gnevdv <gnevyshevdv@gmail.com>
2025-02-13 20:14:49 +0300

    delete chars from end

--- rename_files.py
+++ rename_files.py
@@ -1,13 +1,22 @@
 import os
 
 
-def rename_files_in_directory(text, directory):
+def rename_files_in_directory(text, directory, count):
     # Проходим по всем файлам в указанной папке
     for filename in os.listdir(directory):
         # Проверяем, является ли объект файлом
         if os.path.isfile(os.path.join(directory, filename)):
             # Убираем строку "привет" из названия файла
             new_filename = filename.replace(text, "")
+            if count > 0:
+                last_dot_index = new_filename.rfind(".")
+                # Если точка найдена и перед ней есть хотя бы {COUNT} символов
+                if last_dot_index != -1 and last_dot_index >= count:
+                    # Удаляем {COUNT} символов перед последней точкой
+                    new_filename = (
+                        new_filename[: last_dot_index - count]
+                        + new_filename[last_dot_index:]
+                    )
 
             # Если название изменилось, переименовываем файл
             if new_filename != filename:
@@ -20,5 +29,7 @@ def rename_files_in_directory(text, directory):
 # Укажите путь к папке, в которой нужно переименовать файлы
 directory_path = r"F:\test"
 # Текст, который требуется убрать
-text = "sss"
-rename_files_in_directory(text, directory_path)
+text = "test"
+# Сколько символов перед последней точкой убрать
+count_for_end_remove = 0
+rename_files_in_directory(text, directory_path, count_for_end_remove)
